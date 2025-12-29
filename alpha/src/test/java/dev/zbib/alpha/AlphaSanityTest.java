c5ef4614c647ef8a693ea0018c23c69a0e7beca4
zbib <zbib@zbib.dev>
2025-12-29 23:02:35 +0200

    enhance parent pom

---
+++ alpha/src/test/java/dev/zbib/alpha/AlphaSanityTest.java
@@ -0,0 +1,11 @@
+package dev.zbib.alpha;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlphaSanityTest {
+    @Test
+    void sanityCheck() {
+        assertTrue(true);
+    }
+}
---
+++ beta/src/test/java/dev/zbib/beta/BetaSanityTest.java
@@ -0,0 +1,11 @@
+package dev.zbib.beta;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BetaSanityTest {
+    @Test
+    void sanityCheck() {
+        assertTrue(true);
+    }
+}
